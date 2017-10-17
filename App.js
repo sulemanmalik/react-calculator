d0eeb4b38ad4b46e7b4f0f46c2d289cc55566441
Suleman Malik <sulemanmalik@vpn74233.vpn.mcgill.ca>
2017-10-17 09:08:34 -0400

    Adding input buttons for display layout

--- App.js
+++ App.js
@@ -1,32 +1,61 @@
 import React, {Component} from 'react';
 import Style from './Style';
+import InputButton from './InputButton';
 import {
     StyleSheet,
     Text,
     View,
     AppRegistry} from 'react-native';
 
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
+
 export default class ReactCalculator extends React.Component {
-  render() {
-    return (
-        <View style={{flex: 1}}>
-            <View style={Style.displayContainer}></View>
-            <View style={Style.inputContainer}></View>
-        </View>
 
+    render() {
+        return (
+            <View style={Style.rootContainer}>
+                <View style={Style.displayContainer}></View>
+                <View style={Style.inputContainer}>
+                    {this._renderInputButtons().map( (views) => {return (<View>{views} </View>);}) }
+                </View>
+            </View>
+        )
+    }
+s
+    /**
+     * For each row in `inputButtons`, create a row View and add create an InputButton for each input in the row.
+     */
+    _renderInputButtons() {
+        let views = [];
 
-    );
-  }
+        for (var r = 0; r < inputButtons.length; r ++) {
+            let row = inputButtons[r];
+
+            let inputRow = [];
+            for (var i = 0; i < row.length; i ++) {
+                let input = row[i];
+
+                inputRow.push(
+                    <InputButton value={input} key={r + "-" + i} />
+                );
+            }
+
+            views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+        }
+
+        return views;
+    }
+
 }
+
+
 
 AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
---
+++ InputButton.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sulemanmalik on 2017-10-16.
+ */
+import React, {Component} from 'react';
+import{
+    View,
+    Text
+} from 'react-native';
+import Style from './Style';
+
+export default class InputButton extends Component {
+    render() {
+        <View style={Style.inputButton}>
+            <Text style={Style.inputButtonText}>{this.props.value}</Text>
+        </View>
+    }
+}
